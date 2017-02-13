20517d081c5c21ae99681632c01bed56eb9fd512
simon <psiplace@netscape.net>
2017-02-13 01:43:10 +0000

    upload of raw project

---
+++ flag.go
@@ -0,0 +1,28 @@
+package persist
+
+import "os"
+import "github.com/splace/os/xattribs"
+import "flag"
+
+var userFlagsFile xattribs.FileNS
+
+func init() {
+	thisFile, err := os.Open(os.Args[0])
+	if err != nil {
+		panic(err)
+	}
+	userFlagsFile = xattribs.FileNS{thisFile, "user.flags"}
+}
+
+func FlagSet(fs *flag.FlagSet) {
+	fs.VisitAll(func(f *flag.Flag) {
+		bs, err := userFlagsFile.Get(f.Name)
+		if err == nil {
+			f.Value.Set(string(bs))
+		}else{
+			if f.Value.String()!=f.DefValue {
+				userFlagsFile.Set(f.Name, []byte(f.Value.String()))
+			}
+		}
+	})
+}
